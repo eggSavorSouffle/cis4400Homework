0ceb17a91ce53811532a97cb86fc950da6a592e8
Xavier Souffront <51895552+eggSavorSouffle@users.noreply.github.com>
2024-03-23 19:05:32 -0400

    Update 3/23

--- scripts/playground-1.mongodb.js
+++ scripts/playground-1.mongodb.js
@@ -1,45 +1,5 @@
-/* global use, db */
-// MongoDB Playground
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-
-const database = 'data_storage';
-const collection = 'bedBugData';
-
-// Create a new database.
-use(database);
-
-// Create a new collection.
-db.createCollection(collection);
-
-db.bedBugData.insert('C:\Users\16466\Desktop\cis4400Homework-1\scripts\bedBugData.csv')
-
-// The prototype form to create a collection:
-/* db.createCollection( <name>,
-  {
-    capped: <boolean>,
-    autoIndexId: <boolean>,
-    size: <number>,
-    max: <number>,
-    storageEngine: <document>,
-    validator: <document>,
-    validationLevel: <string>,
-    validationAction: <string>,
-    indexOptionDefaults: <document>,
-    viewOn: <string>,
-    pipeline: <pipeline>,
-    collation: <document>,
-    writeConcern: <document>,
-    timeseries: { // Added in MongoDB 5.0
-      timeField: <string>, // required for time series collections
-      metaField: <string>,
-      granularity: <string>,
-      bucketMaxSpanSeconds: <number>, // Added in MongoDB 6.3
-      bucketRoundingSeconds: <number>, // Added in MongoDB 6.3
-    },
-    expireAfterSeconds: <number>,
-    clusteredIndex: <document>, // Added in MongoDB 5.3
-  }
-)*/
-
-// More information on the `createCollection` command can be found at:
-// https://www.mongodb.com/docs/manual/reference/method/db.createCollection/
+mongoimport \
+   --collection='bedBugData' \
+   --file=bedBugData.csv \
+   --type=csv \
+   --fieldFile= field_file.txt
