deb1832f0f63b1e4b2b598b4c7d390b2423c5296
Xavier Souffront <51895552+eggSavorSouffle@users.noreply.github.com>
2024-03-29 13:41:48 -0400

    converted to gcloud

--- scripts/uploadData.py
+++ scripts/uploadData.py
@@ -1,40 +1,34 @@
-import csv
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from datetime import datetime
 
-# Replace the placeholder with your Atlas connection string
-uri = "mongodb+srv://xavier-souffront:0U5jY8WuzhIPOMSM@gcpcluster.rwcznai.mongodb.net/"
+from google.cloud import storage
+from google.oauth2 import service_account
 
-# Set the Stable API version when creating a new client
-client = MongoClient(uri, server_api=ServerApi('1'))
-                          
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+credentials = service_account.Credentials.from_service_account_file(r"C:\Users\16466\Desktop\cis4400Homework\modified-shape-418121-5e39251205f6.json")
+storage_client = storage.Client(credentials=credentials)
 
 
-#####
-db = client['data_storage']
-collection = db['bedBugData']
-header = ['registration_id', 'building_id','borough',
-'house_number','street_name', 'postcode',
-'of_dwelling_units','infested_dwelling_unit_count', 'eradicated_unit_count',
-'re_infested_dwelling_unit','filing_date', 'filing_period_start_date',
-'filling_period_end_date', 'latitude', 'longitude',
-'community_board','city_council_district', 'census_tract_2010',
-'bin','bbl','nta']
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    bucket_name = "de_hw_bucket"
+    # The path to your file to upload
+    source_file_name = "C:\\Users\\16466\\Desktop\\cis4400Homework\\Data Sources\\bedBugData.csv"
+    # The ID of your GCS object
+    destination_blob_name = "destination_blob.csv"
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
 
-csvFile = open('/Users/16466/Desktop/cis4400Homework/scripts/bedBugData.csv','r')
-reader = csv.DictReader(csvFile)
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
 
-for x in reader:
-    row = {}
-    for field in header:
-        row[field] = x[field]
-    print(row)
-    collection.insert_one(row)
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
 
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+
+
+upload_blob("de_hw_bucket", "C:\\Users\\16466\Desktop\cis4400Homework\\Data Sources\\bedBugData.csv", "modified-shape-418121")
